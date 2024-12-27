074352d586803d3068fa9b1e641afedbd760c52b
unknown <developer.nitish2016@gmail.com>
2024-12-27 07:44:52 +0530

    first commit

---
+++ app/Http/Controllers/RazorpayPaymentController.php
@@ -0,0 +1,50 @@
+<?php
+  
+namespace App\Http\Controllers;
+  
+use Illuminate\Http\Request;
+use Razorpay\Api\Api;
+use Session;
+use Exception;
+  
+class RazorpayPaymentController extends Controller
+{
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function index()
+    {        
+        return view('razorpayView');
+    }
+  
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+  
+        $api = new Api("rzp_test_8njhfyFlqdUgRg", "3KqGnEdxXXKgc80Vf8DJNOx9");
+  
+        $payment = $api->payment->fetch($input['razorpay_payment_id']);
+  
+        if(count($input)  && !empty($input['razorpay_payment_id'])) {
+            try {
+                $response = $api->payment->fetch($input['razorpay_payment_id'])->capture(array('amount'=>$payment['amount'])); 
+  
+            } catch (Exception $e) {
+                return  $e->getMessage();
+                Session::put('error',$e->getMessage());
+                return redirect()->back();
+            }
+        }
+          
+        Session::put('success', 'Payment successful');
+        return redirect()->back();
+    }
+}
+
